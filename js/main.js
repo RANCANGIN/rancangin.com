15d24bf4980319ca81e873c619fd04b8b438dcba
Rowena Ipsilanti <rowena@rowenaipsilanti.com>
2020-04-27 16:33:57 -0700

    Removed jquery.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 function rotate (className, degreeGranularity) {
 
-    var $rota = $(className),
+    var $rota = document.querySelector(className),
         degree = 0;
 
     function spin() {
-        $rota.css({ transform: 'rotate(' + degree + 'deg)'});
+        $rota.style.transform = 'rotate(' + degree + 'deg)';
 		let plusOrMinus = Math.random() < 0.5 ? -1 : 1;
 		let randAngle = degreeGranularity * plusOrMinus;
         let randDelay = Math.floor(3000 + Math.random() * 2000);
